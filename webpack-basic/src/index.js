e2cba84101136bd78377d5517264b8b1aa9ddb44
songguanghui <1554095532@qq.com>
2021-01-04 16:27:39 +0800

    debug

--- webpack-basic/src/index.js
+++ webpack-basic/src/index.js
@@ -1,10 +1,21 @@
-const style = require('./style.css')
-console.log(style)
+import './style.css'
+import Maple from './fengye.jpg'
+import printMe from './print'
 
 function component() {
   const element = document.createElement('div');
-  element.innerHTML = 'webpack basic';
+  element.innerHTML = 'webpack-basic';
   element.className = 'box';
+
+  const img = new Image(100, 57);
+  img.src = Maple;
+  element.appendChild(img);
+
+  const btn = document.createElement('button');
+  btn.innerHTML = '点击按钮';
+  btn.onclick = printMe;
+  element.appendChild(btn);
+
   return element;
 }
 
--- webpack-basic/webpack.config.js
+++ webpack-basic/webpack.config.js
@@ -1,16 +1,50 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 
 module.exports = {
   entry: './src/index.js',
   output: {
-    filename: 'bundle.js',
+    filename: '[name].[hash:8].js',
     path: path.resolve(__dirname, 'dist')
   },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'webpack basic',
+      filename: 'admin.html',
+      template: 'src/index.html'
+    })
+  ],
   module: {
     rules: [
       {
         test: /\.css$/,
-        use: ['css-loader']
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|svg|jpg|gif)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[hash:8].[ext]',
+              outputPath: 'images/'
+            }
+          }
+        ]
+      },
+      {
+        test: /\.(woff|woff2|eot|ttf|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[hash:8].[ext]',
+              outputPath: 'fonts/'
+            }
+          }
+        ]
       }
     ]
   }
